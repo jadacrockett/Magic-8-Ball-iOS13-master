54e36d4ebdd6de77740c4e2b01387f0e3f191637
Jada Crockett <jada.s.crockett@gmail.com>
2024-04-09 13:28:47 -0400

    Randomized button press

--- Magic 8 Ball/ViewController.swift
+++ Magic 8 Ball/ViewController.swift
@@ -16,11 +16,12 @@ class ViewController: UIViewController {
         super.viewDidLoad()
     }
     
-    @IBAction func askButtonPressed(_ sender: UIButton) {
-        imageView.image = UIImage(imageLiteralResourceName: "ball3")
-    }
+
     let ballArray = [#imageLiteral(resourceName: "ball1.png"),#imageLiteral(resourceName: "ball2.png"),#imageLiteral(resourceName: "ball3.png"),#imageLiteral(resourceName: "ball4.png"),#imageLiteral(resourceName: "ball5.png")]
 
+    @IBAction func askButtonPressed(_ sender: UIButton) {
+        imageView.image = ballArray[Int.random(in: 0...4)]
+    }
 
 
 }
